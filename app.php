63430e9e0aa463b8bfaa312eaf0a198b8662f0ce
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-19 13:39:51 +0000

    Initial commit - codeCademy built-in functions project.

---
+++ app.php
@@ -0,0 +1,5 @@
+<!-- Each step of the project provides a math operation that can be completed using built-in PHP functions. Use the documentation to find them and apply them correctly to get the answer. -->
+
+<?php
+$initial = '555';
+
